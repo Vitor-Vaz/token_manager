289e6f0190de670926ee78968c9b2011a95a1ac4
vitor <vitor.vaz.andrade@gmail.com>
2025-12-17 21:04:34 -0300

    chore: remove unused features and configs

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
 import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :token_manager, TokenManager.Repo,
   username: "postgres",
   password: "postgres",
@@ -13,29 +8,14 @@ config :token_manager, TokenManager.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :token_manager, TokenManagerWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "qyrPtY3nMziqFVqcP1SUNkYDQr3j5luVto/Ab6+LDIdTzDMoYQlkXbR6UiwQ7dcr",
   server: false
 
-# In test we don't send emails
-config :token_manager, TokenManager.Mailer, adapter: Swoosh.Adapters.Test
-
-# Disable swoosh api client as it is only required for production adapters
-config :swoosh, :api_client, false
-
-# Print only warnings and errors during test
 config :logger, level: :warning
 
-# Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
 
-# Enable helpful, but potentially expensive runtime checks
-config :phoenix_live_view,
-  enable_expensive_runtime_checks: true
-
-# Sort query params output of verified routes for robust url comparisons
 config :phoenix,
   sort_verified_routes_query_params: true
--- lib/token_manager_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule TokenManagerWeb.PageController do
-  use TokenManagerWeb, :controller
-
-  def home(conn, _params) do
-    render(conn, :home)
-  end
-end
